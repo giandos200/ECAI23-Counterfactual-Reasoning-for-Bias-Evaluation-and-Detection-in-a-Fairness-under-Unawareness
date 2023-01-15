d11f2c9b39c173d58f304a00132d4a8dd489488e
giandomenico <giando95menico@gmail.com>
2023-01-15 18:59:32 +0100

    FirstCommit: MACE dataloading

--- mainEvaluate.py
+++ mainEvaluate.py
@@ -5,7 +5,9 @@ if __name__ == '__main__':
     for dataset in os.listdir('Results'):
         Results = {}
         for model in os.listdir(f'Results/{dataset}'):
-            Results[model] = ''
-            for SF in os.listdir(f'Results/{dataset}/{model}'):
-                Results[model] = f'Results/{dataset}/{model}/{SF}/Genetic.pickle'
+            if os.path.isdir(f'Results/{dataset}/{model}'):
+                Results[model] = ''
+                for SF in os.listdir(f'Results/{dataset}/{model}'):
+                    if os.path.isdir(f'Results/{dataset}/{model}/{SF}'):
+                        Results[model] = f'Results/{dataset}/{model}/{SF}/Genetic.pickle'
         CFmetrics(Results, 'genetic', SF)
